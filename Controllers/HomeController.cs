9b80b9b238418e7c8ff902fc469e188d1e903f2b
Nilay Kothari <nilay.kothari@zealousys.local>
2016-04-05 15:59:41 +0530

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        // GET: Home
+        public ActionResult Index()
+        {
+            log.Debug("Application started");
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(StackMVC.Startup))]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "Web.config", Watch = true)]
+namespace StackMVC
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
